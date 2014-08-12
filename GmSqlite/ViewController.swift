29558a4e78e0629fd4ece01d42aca6f81f890c6a
Masamoto Miyata <miyata@sincere-co.com>
2014-08-13 00:56:56 +0900

    Initial Commit

---
+++ GmSqlite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GmSqlite
+//
+//  Created by Masamoto Miyata on 2014/08/13.
+//  Copyright (c) 2014年 Masamoto Miyata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
